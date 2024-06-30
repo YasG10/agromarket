d69a6c91282c5cd3ae562972ddf9d52f9b85fe71
Yasser Giraldo <yasseryasg@gmail.com>
2024-06-30 07:15:55 -0400

    mod

--- agromarket/settings.py
+++ agromarket/settings.py
@@ -86,11 +86,10 @@ WSGI_APPLICATION = "agromarket.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default="postgresql://postgres:postgres@localhost/postgres", conn_max_age=600
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
     )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
